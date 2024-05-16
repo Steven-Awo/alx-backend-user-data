a72297d7d8c6963f818a7c5bf0fa3cd4da2107d3
StevenAwo <rotimiawomodu@gmail.com>
2024-05-16 14:50:13 +0100

    task 0

--- filtered_logger.py
+++ filtered_logger.py
@@ -7,6 +7,32 @@ import re
 
 from typing import List
 
+import logging
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Creating theRedacting Formatter's class
+    """
+
+    REDACTION = "***"
+
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ initializing the class attributes """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Creating the format record's function """
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """The returning of the log's message that has
