9c045b722e85014e9cd6e47df9ce588b93c74bcd
StevenAwo <rotimiawomodu@gmail.com>
2024-05-16 15:01:09 +0100

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,42 +1,27 @@
 #!/usr/bin/env python3
 """
-    RedactingFormatter
+    Te setting up of the RedactingFormatter
 """
-import csv
-import logging
-import os
+
 import re
+
 from typing import List
-import mysql.connector
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Creating theRedacting Formatter's class
-    """
-
-    REDACTION = "***"
-
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """ initializing the class attributes """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ Creating the format record's function """
-        return filter_datum(self.fields, self.REDACTION,
-                            super(RedactingFormatter, self).format(record),
-                            self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ returns the log message obfuscated """
+    """ The returning of the log message's that were
+    obfuscated
+    
+    Args:
+        fields: a list of strings representing all fields
+        to obfuscate
+        redaction: a string representing by what the
+        field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is
+        separating all fields in the log line (message)
+    """
     return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
                   lambda match: match.group(1) + "=" + redaction
                   if match.group(1) in fields else match.group(0), message)
