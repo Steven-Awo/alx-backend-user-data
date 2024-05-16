aed0a834b191e0d7605eb9a06a44be8f7ff2554a
StevenAwo <rotimiawomodu@gmail.com>
2024-05-16 14:38:42 +0100

    task 0

---
+++ filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Setting up the RedactingFormatter
+"""
+
+import re
+
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ returns the log message obfuscated """
+    return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
+                  lambda match: match.group(1) + "=" + redaction
+                  if match.group(1) in fields else match.group(0), message)
