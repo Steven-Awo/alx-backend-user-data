094fb4af1093ee369cc3910d6e79dace364b1d8c
StevenAwo <rotimiawomodu@gmail.com>
2024-05-16 15:42:13 +0100

    task 5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    The encryption of a password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """It expects to receive one string's argument name of
+    the password and then it returns a salted, hashed
+    password, to which is then a byte's string.
+
+    Args:
+        password (str): the password to be encrypted
+
+    Returns:
+        bytes: type of encryption password
+    """
+    if password:
+        return bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
