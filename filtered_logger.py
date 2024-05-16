2860ceda78a6cadb8c6d23be5b5baa21cd751efb
StevenAwo <rotimiawomodu@gmail.com>
2024-05-16 14:43:04 +0100

    task 0

--- filtered_logger.py
+++ filtered_logger.py
@@ -9,7 +9,18 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ returns the log message obfuscated """
+    """The returning of the log's message that has
+    been obfuscated 
+
+    Args:
+        fields: a list of strings representing all fields
+        to obfuscate
+        redaction: a string representing by what the field
+        will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is
+        separating all fields in the log line (message)
+    """
     return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
                   lambda match: match.group(1) + "=" + redaction
                   if match.group(1) in fields else match.group(0), message)
