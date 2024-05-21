97283e888ba79a1ccb701edbe9b070f54b046a43
StevenAwo <rotimiawomodu@gmail.com>
2024-05-21 16:23:38 +0100

    Task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-"""manage the API authentication"""
+"""The management of the API authentication"""
 from api.v1.auth.auth import Auth
 
 import base64
+
+from typing import TypeVar
+
+from models.user import User
 
 
 class BasicAuth(Auth):
