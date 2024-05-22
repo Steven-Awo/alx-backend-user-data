a7e54be880a149581327317bb99f37f33727b0e0
StevenAwo <rotimiawomodu@gmail.com>
2024-05-22 20:11:58 +0100

    Task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Setting up the session Authentication's module """
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """ Creating a sessionAuth's class that just inherits
+    from the Auth """
+    pass
+
