02720e30d9e459866cad9b8c8a361e2139a58b0f
StevenAwo <rotimiawomodu@gmail.com>
2024-05-22 20:04:44 +0100

    Task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -70,6 +70,7 @@ def before_request() -> None:
 
     if not auth.current_user(request):
         abort(403)
+    
     request.current_user = auth.current_user(request)
 
 if __name__ == "__main__":
