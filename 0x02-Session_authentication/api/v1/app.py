252982dd003a249023a0023ac3cbedfe6bb88113
StevenAwo <rotimiawomodu@gmail.com>
2024-05-22 20:54:48 +0100

    Task 1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,6 +33,11 @@ elif os.getenv('AUTH_TYPE') == 'basic_auth':
 
     auth = BasicAuth()
 
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+
+    auth = SessionAuth()
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
