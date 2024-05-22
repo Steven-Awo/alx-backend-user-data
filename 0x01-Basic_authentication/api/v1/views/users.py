cb35c91f0f4767c12968fafe093f9da924cb46e8
StevenAwo <rotimiawomodu@gmail.com>
2024-05-22 19:48:50 +0100

    Task 0

--- 0x01-Basic_authentication/api/v1/views/users.py
+++ 0x01-Basic_authentication/api/v1/views/users.py
@@ -29,11 +29,8 @@ def view_one_user(user_id: str = None) -> str:
       JSON represented
       - 404 thats if the User's ID doesn't actually exist
     """
-    if user_id == 'me' and user_id is None:
+    if user_id is None:
         abort(404)
-
-    elif user_id == 'me' and request.current_user:
-        return jsonify(request.current_user.to_json())
 
     userr = User.get(user_id)
 
