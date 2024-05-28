53f8e3b6a0469a774a28d2d79b87ab5716a6df51
StevenAwo <rotimiawomodu@gmail.com>
2024-05-28 17:40:37 +0100

    TASK 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,13 +3,10 @@
 The creation of the Flask app
 """
 from auth import Auth
-
 from flask import Flask, jsonify, request
 
 AUTH = Auth()
-
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello() -> str:
@@ -20,21 +17,21 @@ def hello() -> str:
     """
     return jsonify({"message": "Bienvenue"}), 200
 
-
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def user() -> str:
-    """POST route for user register
+    """POST that's the route for user registration
 
     Returns:
-        str: messege
+        str: the message
     """
     email = request.form.get('email')
     password = request.form.get('password')
+
     try:
         AUTH.register_user(email, password)
-        return jsonify({"email": f"{email}", "message": "user created"}), 200
+        return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
-        return jsonify({"messege": "email already registered"}), 400
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
