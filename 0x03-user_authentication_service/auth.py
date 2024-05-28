bf0b32f6b056c0f2e8f53faa85c90b582c7128c7
StevenAwo <rotimiawomodu@gmail.com>
2024-05-28 17:51:46 +0100

    TASK 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """
 Creating the auth's module
 """
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 
 from db import DB
 
@@ -45,3 +45,20 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """The validation login foer a user
+
+        Args:
+            email (str): the user's email
+            password (str): the user's password
+
+        Returns:
+            bool: [description]
+        """
+        try:
+            userr = self._db.find_user_by(email=email)
+
+        except NoResultFound:
+            return False
+        return checkpw(password.encode('utf-8'), userr.hash_password)
