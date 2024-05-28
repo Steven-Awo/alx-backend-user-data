375af7d162434a91ab19e76947ca3a4a87ed494c
StevenAwo <rotimiawomodu@gmail.com>
2024-05-28 15:39:31 +0100

    TASK 6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -16,4 +16,4 @@ def hello() -> str:
     return jsonify({"message": "Bienvenue"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port="5000")
