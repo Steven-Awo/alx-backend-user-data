a2f3b8ee0f5616f83c0c11541e528e084a6e1d3c
StevenAwo <rotimiawomodu@gmail.com>
2024-05-28 15:19:50 +0100

    TASK 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,3 +16,28 @@ def _hash_password(password: str) -> str:
         str: password hashed
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """This is to register a new user privately
+
+        Args:
+            email (str): this is the user's email
+            password (str): this is the user's password
+
+        Returns:
+            User: the user tht was just registered
+        """
+        try:
+            self._db.find_user_by(email=email)
+
+            raise ValueError(f"User {email} already exists")
+
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
