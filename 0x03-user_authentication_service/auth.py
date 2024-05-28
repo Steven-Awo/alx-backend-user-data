a052ab2fcb195116bebc15ab5ebf6ec43ffb868c
StevenAwo <rotimiawomodu@gmail.com>
2024-05-28 19:42:29 +0100

    TASK 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -117,8 +117,8 @@ class Auth:
             user_id (int): the user's id
         """
         try:
-            userr = self._db.find_user_by(id=user_id)
+            user = self._db.find_user_by(id=user_id)
 
-            self._db.update_user(userr.id, session_id=None)
+            self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             pass
