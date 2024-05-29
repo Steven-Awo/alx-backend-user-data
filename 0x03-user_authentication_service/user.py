f6eeee4f168f717612d76a282a40d9c330ca978f
StevenAwo <rotimiawomodu@gmail.com>
2024-05-29 14:02:16 +0100

    TASK 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,27 +3,20 @@
 User model
 """
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy import String, Column, Integer
 
 Base = declarative_base()
 
-
 class User(Base):
-    """The user's class model
+    """The user model class.
 
     Args:
-        Base (class): for the declarative base thats
-        from the sqlalchemy
+        Base (class): Base class for declarative class definitions from SQLAlchemy.
     """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-
     email = Column(String(250), nullable=False)
-
     hashed_password = Column(String(250), nullable=False)
-
     session_id = Column(String(250), nullable=True)
-
     reset_token = Column(String(250), nullable=True)
